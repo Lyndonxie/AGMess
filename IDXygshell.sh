d6c5d52ca29e25047019200f7fd2ad1b05f8a153
Lyndonxie <xieliangkk@outlook.com>
2025-06-24 07:55:55 +0800

    Update and rename IDXygshell to IDXygshell.sh

---
+++ IDXygshell.sh
@@ -0,0 +1,8 @@
+nix=y uuid=8bc74623-6ae8-4d92-bcc2-c63b38adc734 vmpt=50108 agn=lx.xielioukom.x10.mx agk=eyJhIjoiYWExZDM1MDYzNGQ1MDA2OGJkMGZkZDIxNjA3MGZhZTUiLCJ0IjoiYjRiNjZjODktNzg1Yy00NDg5LWE4NDUtNDFhMzY2NjA1ZDk1IiwicyI6Ik16QXpNbVl5WkRndFkyWmxOeTAwTXpoaExXSTVPVEl0TXpKalpHRm1OMk5oWkdGaSJ9 bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosb/main/argosb.sh)
+# --- 其他配置 ---
+export NAME="idx"               # 节点名称
+export CFIP="www.visa.com.hk" # 优选 IP 或优选域名
+export CFPORT=443               # 优选 IP 或优选域名对应端口
+export CHAT_ID="-4918534407"               # Telegram Chat ID
+export BOT_TOKEN="8053333842:AAGuW87kKbZ5Enl3AfezSgwyb0txR-0iTis"             # Telegram Bot Token。需要同时填写 Chat ID 才会推送节点到 Telegram
+export UPLOAD_URL=              # 节点自动推送到订阅器，需要填写部署 merge-sub 项目后的首页地址，例如：https://merge.eooce.ggff.net
