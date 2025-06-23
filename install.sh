b9cd8b9c53dd32292ca18561029dad55ff5afac8
Lyndonxie <xieliangkk@outlook.com>
2025-06-23 22:45:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,12 +14,12 @@ export NEZHA_PORT=""            # v1 哪吒不要填写这个。v0 哪吒 agent 
 export NEZHA_KEY=""             # v1 哪吒的 NZ_CLIENT_SECRET 或 v0 哪吒 agent 密钥
 
 # --- Argo 隧道配置 ---
-export ARGO_DOMAIN=""           # Argo 域名，留空即启用临时隧道
-export ARGO_AUTH=""             # Argo Token 或 json，留空即启用临时隧道
+export ARGO_DOMAIN="lx.x5411y.cloudns.biz"           # Argo 域名，留空即启用临时隧道
+export ARGO_AUTH="eyJhIjoiYWExZDM1MDYzNGQ1MDA2OGJkMGZkZDIxNjA3MGZhZTUiLCJ0IjoiYjRiNjZjODktNzg1Yy00NDg5LWE4NDUtNDFhMzY2NjA1ZDk1IiwicyI6Ik16QXpNbVl5WkRndFkyWmxOeTAwTXpoaExXSTVPVEl0TXpKalpHRm1OMk5oWkdGaSJ9"             # Argo Token 或 json，留空即启用临时隧道
 
 # --- 其他配置 ---
 export NAME="idx"               # 节点名称
-export CFIP="www.visa.com.hk" # 优选 IP 或优选域名
+export CFIP="www.ip.sb" # 优选 IP 或优选域名
 export CFPORT=443               # 优选 IP 或优选域名对应端口
 export CHAT_ID="-4918534407"               # Telegram Chat ID
 export BOT_TOKEN="8053333842:AAGuW87kKbZ5Enl3AfezSgwyb0txR-0iTis"             # Telegram Bot Token。需要同时填写 Chat ID 才会推送节点到 Telegram
