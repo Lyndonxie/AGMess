0092cd108d5bb590c9a1407fe9966b3607956f6a
Lyndonxie <xieliangkk@outlook.com>
2025-06-24 11:20:29 +0800

    Update IDXygshell.sh

--- IDXygshell.sh
+++ IDXygshell.sh
@@ -1,11 +1,11 @@
 vmpt="50108" uuid="dadf0ebf-034a-49cf-9f46-5a48ef01ae60" argo="y" agn="lx.xielioukom.x10.mx" agk="eyJhIjoiYWExZDM1MDYzNGQ1MDA2OGJkMGZkZDIxNjA3MGZhZTUiLCJ0IjoiYjRiNjZjODktNzg1Yy00NDg5LWE4NDUtNDFhMzY2NjA1ZDk1IiwicyI6Ik16QXpNbVl5WkRndFkyWmxOeTAwTXpoaExXSTVPVEl0TXpKalpHRm1OMk5oWkdGaSJ9" bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosb/main/argosb.sh)
-test=cat /home/user/agsb/jh.txt
+cat /home/user/agsb/jh.txt
 # 设置 Bot API token
 BOT_TOKEN="8053333842:AAGuW87kKbZ5Enl3AfezSgwyb0txR-0iTis"
 # 设置目标群组的 chat ID
 CHAT_ID="-4918534407"
 # 要发送的消息内容
-MESSAGE= test
+MESSAGE= "test"
 
 # 使用 curl 发送消息
 curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE"
