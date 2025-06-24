5bbaa7ad775a5647b5ad43e906571cdcb16dafbc
Lyndonxie <xieliangkk@outlook.com>
2025-06-24 10:46:43 +0800

    Update IDXygshell.sh

--- IDXygshell.sh
+++ IDXygshell.sh
@@ -1,8 +1,11 @@
 nix=y uuid=8bc74623-6ae8-4d92-bcc2-c63b38adc734 vmpt=50108 agn=lx.xielioukom.x10.mx agk=eyJhIjoiYWExZDM1MDYzNGQ1MDA2OGJkMGZkZDIxNjA3MGZhZTUiLCJ0IjoiYjRiNjZjODktNzg1Yy00NDg5LWE4NDUtNDFhMzY2NjA1ZDk1IiwicyI6Ik16QXpNbVl5WkRndFkyWmxOeTAwTXpoaExXSTVPVEl0TXpKalpHRm1OMk5oWkdGaSJ9 bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosb/main/argosb.sh)
-# --- 其他配置 ---
-export NAME="idx"               # 节点名称
-export CFIP="www.visa.com.hk" # 优选 IP 或优选域名
-export CFPORT=443               # 优选 IP 或优选域名对应端口
-export CHAT_ID="-4918534407"               # Telegram Chat ID
-export BOT_TOKEN="8053333842:AAGuW87kKbZ5Enl3AfezSgwyb0txR-0iTis"             # Telegram Bot Token。需要同时填写 Chat ID 才会推送节点到 Telegram
-export UPLOAD_URL=              # 节点自动推送到订阅器，需要填写部署 merge-sub 项目后的首页地址，例如：https://merge.eooce.ggff.net
+
+# 设置 Bot API token
+BOT_TOKEN="8053333842:AAGuW87kKbZ5Enl3AfezSgwyb0txR-0iTis"
+# 设置目标群组的 chat ID
+CHAT_ID="-4918534407"
+# 要发送的消息内容
+MESSAGE="你好，世界！"
+
+# 使用 curl 发送消息
+curl -s -X POST https://api.telegram.<bos>.org/bot$BOT_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE"
