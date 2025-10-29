5d5252ec7f52b0aced4c034e2abfc1c88b15edff
Lyndonxie <xieliangkk@outlook.com>
2025-10-30 04:59:19 +0800

    Add installation script for Warp and dependencies

---
+++ install_warp.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/Lyndonxie/AGMess/refs/heads/main/install_warp.sh
+wget https://raw.githubusercontent.com/Lyndonxie/AGMess/refs/heads/main/auto_append.sh
+wget https://raw.githubusercontent.com/Lyndonxie/AGMess/refs/heads/main/ieltsWords.txt
+bash install_warp.sh
